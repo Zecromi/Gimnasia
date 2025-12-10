a6791c773fae585d14aac60d3b99f7ced3415627
Martz94 <rush-2@live.com.mx>
2025-12-09 23:46:12 -0600

    ajuste middleware

--- middleware.ts
+++ middleware.ts
@@ -10,24 +10,11 @@ export function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/', request.url))
     }
 
-    // If token exists and user is on login page, redirect to dashboard (optional but good UX)
-    // if (token && request.nextUrl.pathname === '/') {
-    //   return NextResponse.redirect(new URL('/dashboard', request.url))
-    // }
-
     return NextResponse.next()
 }
 
 export const config = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - api (API routes)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * - public files (images, svgs, etc)
-         */
         '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
